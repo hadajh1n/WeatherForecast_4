b55d6155f1039b7170d950b5fb5261487a71e2c6
hadajh1n <anton_sheludchenkov@mail.ru>
2025-04-19 17:15:30 +0300

    добавление mvvm и dagger hilt

--- app/src/main/java/com/example/weatherforecast_4/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast_4/MainActivity.kt
@@ -1,37 +1,21 @@
 package com.example.weatherforecast_4
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.weatherforecast_4.retrofit.CurrentWeather
-import com.example.weatherforecast_4.retrofit.WeatherApi
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
-import javax.inject.Inject
 import com.bumptech.glide.Glide
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    val weatherViewModel: WeatherViewModel by viewModels()
+    private val weatherViewModel: WeatherViewModel by viewModels()
 
     private lateinit var cityTextView: TextView
     private lateinit var temperatureTextView: TextView
     private lateinit var descriptionTextView: TextView
     private lateinit var weatherIconImageView: ImageView
-
-    private val apiKey = "3a40caaed30624dd3ed13790e371b4bd"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,36 +26,23 @@ class MainActivity : AppCompatActivity() {
         descriptionTextView = findViewById(R.id.descriptionTextView)
         weatherIconImageView = findViewById(R.id.weatherIconImageView)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.openweathermap.org/data/2.5/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        val weatherApi = retrofit.create(WeatherApi::class.java)
+        weatherViewModel.weather.observe(this) { weather ->
+            weather?.let {
+                cityTextView.text = it.name
+                temperatureTextView.text = "${it.main.temp}°"
+                descriptionTextView.text = it.weather[0].description
+                val iconUrl = "https://openweathermap.org/img/wn/${it.weather[0].icon}@2x.png"
+                Glide.with(this@MainActivity)
+                    .load(iconUrl)
+                    .into(weatherIconImageView)
+            }
+        }
 
-        // Использование Coroutine для фонового потока
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val weather = weatherApi.getCurrentWeather(
-                    city = "Orel",
-                    apiKey = apiKey,
-                    lang = "ru"
-                )
-                runOnUiThread {
-                    cityTextView.text = weather.name
-                    temperatureTextView.text = "${weather.main.temp}°"
-                    descriptionTextView.text = weather.weather[0].description
-                    val iconUrl = "https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png"
-                    Glide.with(this@MainActivity)
-                        .load(iconUrl)
-                        .into(weatherIconImageView)
-                }
-
-            } catch (e: Exception) {
-                runOnUiThread {
-                    cityTextView.text = "Город не найден"
-                    temperatureTextView.text = "Не удалось загрузить данные"
-                    descriptionTextView.text = e.message
-                }
+        weatherViewModel.error.observe(this) { error ->
+            error?.let {
+                cityTextView.text = "Город не найден"
+                temperatureTextView.text = "Не удалось загрузить данные"
+                descriptionTextView.text = it
             }
         }
     }
--- app/src/main/java/com/example/weatherforecast_4/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherforecast_4/WeatherViewModel.kt
@@ -1,11 +1,44 @@
 package com.example.weatherforecast_4
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherforecast_4.retrofit.CurrentWeather
+import com.example.weatherforecast_4.retrofit.WeatherApi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class WeatherViewModel @Inject constructor(): ViewModel() {
+class WeatherViewModel @Inject constructor(private val weatherApi: WeatherApi): ViewModel() {
+    private val _weather = MutableLiveData<CurrentWeather?>()
+    val weather: LiveData<CurrentWeather?> get() = _weather
 
+    private val _error = MutableLiveData<String?>()
+    val error: LiveData<String?> get() = _error
+
+    private val apiKey = "3a40caaed30624dd3ed13790e371b4bd"
+
+    init {
+        fetchWeather("Orel")
+    }
+
+    fun fetchWeather(city: String) {
+        viewModelScope.launch {
+            try {
+                val response = weatherApi.getCurrentWeather(
+                    city = city,
+                    apiKey = apiKey,
+                    lang = "ru"
+                )
+                _weather.postValue(response)
+                _error.postValue(null)
+            } catch (e: Exception) {
+                _weather.postValue(null)
+                _error.postValue("")
+            }
+        }
+    }
 }
